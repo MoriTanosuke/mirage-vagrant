473b425d7d9f560b4dfdeb5e053e27b3c8d03cf0
Carsten Ringe <carsten@kopis.de>
2014-06-08 11:19:24 +0200

    Fix install commands for OPAM.

--- install-mirage.sh
+++ install-mirage.sh
@@ -3,11 +3,10 @@
 sudo apt-get -y update
 sudo apt-get -y install build-essential m4 ocaml ocaml-native-compilers camlp4-extra
 # install opam
-pushd /tmp
+cd /tmp
 wget https://raw.github.com/ocaml/opam/master/shell/opam_installer.sh
-sh ./opam_installer.sh /usr/local/bin
-popd
+sh ./opam_installer.sh /usr/local/bin 4.01.0
 # init opam and install mirage
-opam init
-opam switch 4.01.0
-opam install mirage
+opam init -y
+opam update -y
+opam install -y mirage
